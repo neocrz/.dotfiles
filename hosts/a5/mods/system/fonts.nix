e4ce7a8226029b22e672172846af48da24de0a1f
neocrz <neo59crz@gmail.com>
2024-11-08 15:13:40 -0300

    fonts and aliases

---
+++ hosts/a5/mods/system/fonts.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, dotinfo, ... }:
+{
+  fonts = {
+    enableDefaultPackages = true;
+    fontDir.enable = true;
+
+    packages = with pkgs; [
+      carlito
+      dejavu_fonts
+      ipafont
+      kochi-substitute
+      liberation_ttf
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-emoji
+      source-code-pro
+      ubuntu_font_family
+      unifont
+    ];
+
+    fontconfig = {
+      antialias = true;
+      defaultFonts = {
+        serif = [ "Ubuntu" ];
+        sansSerif = [ "Ubuntu" ];
+        monospace = [ "Ubuntu Source" ];
+      };
+    };
+  }; 
+}
--- mods/home/bash.nix
+++ mods/home/bash.nix
@@ -5,6 +5,8 @@
     shellAliases = {
       mp3 = "yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 ";
       mp4 = "yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 ";
+      no-rs = "sudo nixos-rebuild switch --flake ~/.dotfiles";
+      no-rb = "sudo nixos-rebuild boot --flake ~/.dotfiles";
     };
   };
 }
